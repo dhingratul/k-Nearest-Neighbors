4cfa17541bbb9b3700825b566615046ef8dc8fdc
dhingratul <dhingra.atul92@gmail.com>
2017-09-20 12:45:43 -0400

    Run files

---
+++ run_NN.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 15:42:57 2017
+
+@author: dhingratul
+Run file for Nearest neighbor algorithm
+"""
+import numpy as np
+import kNN as kNN
+
+# Driver
+direc = '/home/dhingratul/Documents/Dataset/cifar-10-batches-py/'
+test_file = 'test_batch'
+filename = 'data_batch_'
+X_train, y_train = kNN.getCIFAR10(direc, filename, 5)
+data_test = kNN.unpickle(direc + test_file)
+X_test = data_test[b'data']
+y_test = data_test[b'labels']
+# Call NN
+nn = kNN.NearestNeighbors()
+nn.train(X_train, y_train)
+Yte_predict = nn.predict(X_test)
+print('accuracy: {}' .format(np.mean(Yte_predict == y_test)))
---
+++ run_knn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 15:42:57 2017
+
+@author: dhingratul
+Run file for k- Nearest neighbor algorithm
+"""
+import numpy as np
+import kNN as kNN
+
+# Driver
+direc = '/home/dhingratul/Documents/Dataset/cifar-10-batches-py/'
+test_file = 'test_batch'
+filename = 'data_batch_'
+X_train, y_train = kNN.getCIFAR10(direc, filename, 5)
+data_test = kNN.unpickle(direc + test_file)
+X_test = data_test[b'data']
+y_test = data_test[b'labels']
+# Call NN
+knn = kNN.kNearestNeighbors()
+k = 5
+knn.train(X_train, y_train, k)
+Yte_predict = knn.predict(X_test)
+print('accuracy: {}' .format(np.mean(Yte_predict == y_test)))
